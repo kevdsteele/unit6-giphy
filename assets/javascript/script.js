d83927f46dcb70e91df2ad23ff273652258b4d3d
Kevin Steele <kevdsteele@gmail.com>
2019-06-16 23:24:35 -0400

    Logic updates

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
 
 var characters= ["Thor", "Spiderman", "Iron Man", "Captain America", "Hulk", "Black Widow", "Black Panther", "Hawkeye"]
+
 var giphyKey = "ebLf7WRabdqXmFc7BjhdTDPDNiP4bA4T";
-var marvelPubKey ="72dfe9ce4a224033d095c4fcf90e07c7";
-var marvelPrivKey ="45f957f0116b59b04efc1c75bbbc302bea8d15f9";
+
 var Search ="";
-var ts= $.now();
+
 
 
 
@@ -45,46 +45,48 @@ function getGiphy(Search) {
     $("#gc"+i).append(ratingDiv);
 
     var favCheckbox = $('<input type ="checkbox" name="' + Search+i + '" class="css-checkbox" id="'+ Search + i + '"> <label for="' + Search+i +'" class="css-label fav"></label>');
-   favCheckbox.attr("gid", i );
-    
+   favCheckbox.attr("gid", response.data[i].id );
+   favCheckbox.attr("clicked", response.data[i].images.fixed_height_small.url);
+   favCheckbox.attr("notClicked", response.data[i].images.fixed_height_small_still.url )
+  favCheckbox.attr("rating", response.data[i].rating)
     $("#gr"+i).append(favCheckbox);
 
 
    
 
-    }
+    };
 
-    $(".giphyBtn").on("click", function () {
-      console.log("giphy clicked");
-    
-      var isClicked =$(this).attr("isClicked");
-      console.log("Is clicked " + isClicked);
-      if (isClicked === "false") {
-      
-          newImg = $(this).attr("clicked");
-          $(this).attr("src", newImg);
-          $(this).attr("isClicked", "true");
-      
-      } else {
-      
-          newImg = $(this).attr("notClicked");
-          $(this).attr("src", newImg);
-          $(this).attr("isClicked", "false");
-      
-      };
-      
-      
-      
-      /*end on click */    
-      });
 
-      $(".fav").on("click", function () {
+
+  $(".fav").on("click", function () {
+  console.log("fav clicked");
   var favSelected = $(this).attr("gid");
-  $("#gc" + favSelected).clone(true).appendTo("#favs");
+  var favStillLink = $(this).attr("notClicked");
+  var favAniLink = $(this).attr("clicked");
+  var favRating = $(this).attr("rating");
+
+ var favExists=false;
+
+for (i=0; i < favCharacters.length; i++) {
+ if (favCharacters[i].CharID === favSelected) {
+   favExists =true;
+
+
+ }
+
+
+}
+
+console.log("does it exist "+ favExists);
+if (!favExists) {
+favCharacters.push({"CharID" : favSelected, "Still": favStillLink, "Ani": favAniLink, "Rating": favRating});
+localStorage.setItem("favorites", JSON.stringify(favCharacters));
 console.log("fav clicked");
+console.log(favCharacters);
 
-
-
+createFavs();
+  
+  }
 });
 
   });
@@ -101,7 +103,45 @@ console.log("fav clicked");
 
 
 
+function createFavs () {
+$("#favs").empty();
+for (i=0;i < favCharacters.length; i++) {
 
+  var containerDiv = $("<div>");
+  containerDiv.addClass("giphyCont");
+  containerDiv.attr("id", "favgc"+i);
+  $("#favs").append(containerDiv);
+
+  var imgDiv = $("<img>");
+  imgDiv.attr("src", favCharacters[i].Still);
+  imgDiv.attr("notClicked", favCharacters[i].Still);
+  imgDiv.attr("clicked",favCharacters[i].Ani);
+  imgDiv.addClass("giphyBtn");
+  imgDiv.attr("isClicked", "false");
+  imgDiv.attr("id", "favgiphy"+i);
+  $("#favgc"+i).append(imgDiv);
+  var ratingDiv = $("<div>");
+  ratingDiv.html("Rated " + favCharacters[i].Rating);
+  ratingDiv.addClass("giphyRating");
+  ratingDiv.attr("id", "favgr"+i);
+  $("#favgc"+i).append(ratingDiv);
+
+ /* var favCheckbox = $('<input type ="checkbox" name="' + Search+i + '" class="css-checkbox" id="'+ Search + i + '"> <label for="' + Search+i +'" class="css-label fav"></label>');
+ favCheckbox.attr("gid", Search+i );
+ favCheckbox.attr("clicked", response.data[i].images.fixed_height_small.url);
+ favCheckbox.attr("notClicked", response.data[i].images.fixed_height_small_still.url )
+  
+  $("#gr"+i).append(favCheckbox);*/
+
+
+/*end loop*/  
+};
+
+
+
+
+/*end create favs*/  
+};
   
 
 
@@ -114,7 +154,7 @@ console.log("fav clicked");
 
 
 
-function createButtons() {
+function createButtons(characters) {
 
 
   $("#buttons").empty();
@@ -154,20 +194,93 @@ function createButtons() {
 
 
 
-createButtons();
+createButtons(characters);
 
 
 
 
-
-$(".char-btn").on("click", function (){
+/* Click button that parses value from button clicked to egt Giphys*/
+$(document).on("click",".char-btn", function (event){
+  event.preventDefault();
 $("#results").empty();
-  Search=$(this).attr("char-name");
+Search=$(this).attr("char-name");
 console.log("char btn clicked");
 getGiphy(Search);
 });
 
+$(document).on("click","#clear", function (event){
+  event.preventDefault();
+$("#favs").empty();
+favCharacters=[];
+localStorage.clear();
 
+});
+
+
+/*Get local array of favorite characters */
+var favCharacters = JSON.parse(localStorage.getItem("favorites"));
+
+
+/* If the list does not exist in local storage declare a new empty array*/
+if (!Array.isArray(favCharacters)) {
+  favCharacters = [];
+}
+
+createFavs(favCharacters);
+
+$("#add").on("click", function () {
+
+var newChar=$("#add-text").val().trim();
+
+for (i=0; i < characters.length; i++) {
+if (newChar === characters[i]) {
+var charExists= true;
+
+alert("Character button already created");
+
+} 
+
+
+
+
+
+}
+
+if (!charExists) {
+  characters.push(newChar);
+  createButtons(characters);
+ $("#add-text").val("");
+}
+
+
+
+
+});
+
+$(document).on("click", ".giphyBtn", function(event){
+  event.preventDefault();
+  console.log("giphy clicked");
+
+  var isClicked =$(this).attr("isClicked");
+  console.log("Is clicked initial status " + isClicked);
+  if (isClicked === "false") {
+  
+      newImg = $(this).attr("clicked");
+      $(this).attr("src", newImg);
+      $(this).attr("isClicked", "true");
+  
+  } else {
+  
+      newImg = $(this).attr("notClicked");
+      $(this).attr("src", newImg);
+      $(this).attr("isClicked", "false");
+  
+  };
+  
+  
+  
+  /*end on click */    
+  });
 
 /* end doc ready function */    
 });
